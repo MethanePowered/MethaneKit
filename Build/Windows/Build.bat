2736b5f4a749970f720ccb101ef3a8b4d11540bc
Evgeny Gorodetsky <evsgor@gmail.com>
2019-05-28 22:57:44 +0300

    Fixed Sonar Scanner code analysis in Windows build script

--- Build/Windows/Build.bat
+++ Build/Windows/Build.bat
@@ -4,6 +4,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET PLATFORM_TYPE=Win64
 SET CONFIG_TYPE=Release
+SET BUILD_VERSION=0.1
 
 SET CONFIG_DIR=%~dp0..\Output\VisualStudio\%PLATFORM_TYPE%-%CONFIG_TYPE%
 SET INSTALL_DIR=%CONFIG_DIR%\Install
@@ -14,6 +15,10 @@ IF "%~1"=="--analyze" (
 
     SET IS_ANALYZE_BUILD=1
     SET BUILD_DIR=%CONFIG_DIR%\Analyze
+    SET SONAR_SCANNER_DIR=%SOURCE_DIR%\Externals\SonarScanner\binaries\Windows
+    SET SONAR_SCANNER_ZIP=!SONAR_SCANNER_DIR!.zip
+    SET SONAR_BUILD_WRAPPER_EXE=!SONAR_SCANNER_DIR!\build-wrapper-win-x86-64.exe
+    SET SONAR_SCANNER_MSBUILD_EXE=!SONAR_SCANNER_DIR!\SonarScanner.MSBuild.exe
 
     ECHO =========================================================
     ECHO Code analysis for build Methane %PLATFORM_TYPE% %CONFIG_TYPE%
@@ -36,50 +41,74 @@ IF "%~1"=="--analyze" (
 
 ECHO Pulling latest changes with submodules...
 git pull --recurse-submodules
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+RD /S /Q "%BUILD_DIR%"
+MKDIR "%BUILD_DIR%"
 
 ECHO ---
 
 IF %IS_ANALYZE_BUILD% EQU 1 (
+    ECHO Unpacking Sonar Scanner binaries...
+    IF EXIST "%SONAR_SCANNER_DIR%" RD /S /Q "%SONAR_SCANNER_DIR%"
+    CALL powershell -Command "Expand-Archive %SONAR_SCANNER_ZIP% -DestinationPath %SONAR_SCANNER_DIR%"
+    IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-ECHO Analyzing code with Sonar Scanner http://www.sonarcloud.io...
-ECHO ON
+    SET GITBRANCH=
+    FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --abbrev-ref HEAD`) DO (
+        IF NOT "%%F" == "" SET GITBRANCH=%%F
+    )
+    IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-@CD "%BUILD_DIR%"
-cmake -G "Visual Studio 15 2017 %PLATFORM_TYPE%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% "%SOURCE_DIR%"
-@IF %ERRORLEVEL% NEQ 0 GOTO STOP
-@CD "%SOURCE_DIR%"
+    ECHO ---
+    ECHO Analyzing code with Sonar Scanner on branch !GITBRANCH!...
 
-SonarScanner.MSBuild.exe begin^
-    /k:egorodet_MethaneKit^
-    /o:egorodet-github^
-    /d:sonar.sources="%SOURCE_DIR%"^
-    /d:sonar.projectBaseDir="%SOURCE_DIR%"^
-    /d:sonar.host.url="https://sonarcloud.io"^
-    /d:sonar.login=6e1dbce6af614f59d75f1d78f0609aaaa60caee1"
-@IF %ERRORLEVEL% NEQ 0 GOTO STOP
+    IF NOT EXIST "%BUILD_DIR%" MKDIR "%BUILD_DIR%"
+    CD "%BUILD_DIR%"
 
-MSBuild.exe "%BUILD_DIR%\MethaneKit.sln" /t:Rebuild
-@IF %ERRORLEVEL% NEQ 0 GOTO STOP
+    "%SONAR_BUILD_WRAPPER_EXE%" --out-dir "%BUILD_DIR%"^
+         cmake -G "Visual Studio 15 2017 %PLATFORM_TYPE%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% "%SOURCE_DIR%"
+    IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-SonarScanner.MSBuild.exe end^
-    /d:sonar.login=6e1dbce6af614f59d75f1d78f0609aaaa60caee1
+    CD "%SOURCE_DIR%"
+    "%SONAR_SCANNER_MSBUILD_EXE%" begin^
+        /k:egorodet_MethaneKit^
+        /o:egorodet-github^
+        /v:%BUILD_VERSION%^
+        /d:sonar.branch.name="!GITBRANCH!"^
+        /d:sonar.sources="%SOURCE_DIR%"^
+        /d:sonar.projectBaseDir="%SOURCE_DIR%"^
+        /d:sonar.cfamily.build-wrapper-output="%BUILD_DIR%"^
+        /d:sonar.host.url="https://sonarcloud.io"^
+        /d:sonar.login=6e1dbce6af614f59d75f1d78f0609aaaa60caee1
+    IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+    MSBuild.exe "%BUILD_DIR%\MethaneKit.sln" /t:Rebuild
+    IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+    "%SONAR_SCANNER_MSBUILD_EXE%" end^
+        /d:sonar.login=6e1dbce6af614f59d75f1d78f0609aaaa60caee1
+    IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 ) ELSE (
+    CD "%BUILD_DIR%"
 
-RD /S /Q "%CONFIG_DIR%"
-MKDIR "%BUILD_DIR%"
-CD "%BUILD_DIR%"
+    ECHO Building with Visual Studio 2017...
 
-ECHO Building with Visual Studio 2017...
-ECHO ON
+    cmake -G "Visual Studio 15 2017 %PLATFORM_TYPE%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% "%SOURCE_DIR%"
+    IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-cmake -G "Visual Studio 15 2017 %PLATFORM_TYPE%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% "%SOURCE_DIR%"
-@IF %ERRORLEVEL% NEQ 0 GOTO STOP
-
-cmake --build . --config %CONFIG_TYPE% --target install
-@IF %ERRORLEVEL% NEQ 0 GOTO STOP
-
+    cmake --build . --config %CONFIG_TYPE% --target install
+    IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 )
 
+GOTO STOP
+
+:ERROR
+ECHO Error occurred %ERRORLEVEL%. Script execution was stopped.
+GOTO STOP
+
 :STOP
-@CD "%START_DIR%"
+CD "%START_DIR%"
+ENDLOCAL
+ECHO ON
