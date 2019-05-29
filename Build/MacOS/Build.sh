c4dc0459cf5164027e2f8ca866fa8773242afd73
Evgeny Gorodetsky <evsgor@gmail.com>
2019-05-29 17:05:19 +0300

    Updated build analysis script for MacOS to use Sonar Scanner binaries from Externals repository

--- Build/MacOS/Build.sh
+++ Build/MacOS/Build.sh
@@ -12,13 +12,21 @@ do
 done
 
 CONFIG_TYPE=Release
+BUILD_VERSION=0.1
+
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 SOURCE_DIR=$SCRIPT_DIR/../..
 OUTPUT_DIR=$SCRIPT_DIR/../Output/XCode
 INSTALL_DIR=$OUTPUT_DIR/Install
 
 if [ "$IS_ANALYZE_BUILD" == true ]; then
+
     BUILD_DIR=$OUTPUT_DIR/Analyze
+    SONAR_SCANNER_DIR=$SOURCE_DIR/Externals/SonarScanner/binaries/MacOS
+    SONAR_SCANNER_ZIP=$SONAR_SCANNER_DIR.zip
+    SONAR_BUILD_WRAPPER_EXE=$SONAR_SCANNER_DIR/bin/build-wrapper-macosx-x86
+    SONAR_SCANNER_EXE=$SONAR_SCANNER_DIR/bin/sonar-scanner
+
     echo =========================================================
     echo Code analysis for build Methane $CONFIG_TYPE
     echo =========================================================
@@ -43,16 +51,27 @@ git pull --recurse-submodules
 echo ---
 
 if [ "$IS_ANALYZE_BUILD" == true ]; then
-    echo Analyzing code with Sonar Scanner http://www.sonarcloud.io...
-    build-wrapper-macosx-x86 --out-dir "$BUILD_DIR" \
+
+    echo Unpacking Sonar Scanner binaries...
+    rm -rf "$SONAR_SCANNER_DIR"
+    unzip -q "$SONAR_SCANNER_ZIP" -d "$SONAR_SCANNER_DIR"
+
+    GITBRANCH=$(git symbolic-ref --short HEAD)
+
+    echo Analyzing code with Sonar Scanner on branch $GITBRANCH...
+    "$SONAR_BUILD_WRAPPER_EXE" --out-dir "$BUILD_DIR" \
         cmake -H"$SOURCE_DIR" -B"$BUILD_DIR" -G Xcode
-    sonar-scanner \
+
+    "$SONAR_SCANNER_EXE" \
         -Dsonar.projectKey=egorodet_MethaneKit \
         -Dsonar.organization=egorodet-github \
+        -Dsonar.branch.name=$GITBRANCH \
+        -Dsonar.projectVersion=$BUILD_VERSION \
         -Dsonar.sources="$SOURCE_DIR" \
         -Dsonar.cfamily.build-wrapper-output="$BUILD_DIR" \
         -Dsonar.host.url=https://sonarcloud.io \
-        -Dsonar.login=6e1dbce6af614f59d75f1d78f0609aaaa60caee1 \
+        -Dsonar.login=6e1dbce6af614f59d75f1d78f0609aaaa60caee1
+
 else
     echo Building with XCode...
     cmake -H"$SOURCE_DIR" -B"$BUILD_DIR" -G Xcode -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR"
