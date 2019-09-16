57f436f3fe06d1f28a10c2fdbb0dd3d78d5cad79
Evgeny Gorodetsky <evsgor@gmail.com>
2019-09-16 23:51:28 +0300

    Fixed depth compare function setup in Metal render state.

--- Modules/Graphics/Core/Sources/Methane/Graphics/Metal/RenderStateMT.mm
+++ Modules/Graphics/Core/Sources/Methane/Graphics/Metal/RenderStateMT.mm
@@ -176,7 +176,7 @@ void RenderStateMT::Reset(const Settings& settings)
     // Depth-stencil state
     m_mtl_depth_stencil_state_desc                      = [[MTLDepthStencilDescriptor alloc] init];
     m_mtl_depth_stencil_state_desc.depthWriteEnabled    = m_settings.depth.enabled ? YES : NO;
-    m_mtl_depth_stencil_state_desc.depthCompareFunction = m_settings.depth.enabled ? TypeConverterMT::CompareFunctionToMetal(m_settings.depth.compare) : MTLCompareFunctionNever;
+    m_mtl_depth_stencil_state_desc.depthCompareFunction = TypeConverterMT::CompareFunctionToMetal(m_settings.depth.compare);
     m_mtl_depth_stencil_state_desc.backFaceStencil      = ConvertStencilDescriptorToMetal(m_settings.stencil, false);
     m_mtl_depth_stencil_state_desc.frontFaceStencil     = ConvertStencilDescriptorToMetal(m_settings.stencil, true);
     
