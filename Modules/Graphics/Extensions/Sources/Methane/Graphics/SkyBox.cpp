48cdce3fcfbc4de3363634b5b2495a3aaff8fd5c
Evgeny Gorodetsky <evsgor@gmail.com>
2019-09-16 23:52:03 +0300

    Sky-box rendering implemented in Asteroids sample.

--- Modules/Graphics/Extensions/Sources/Methane/Graphics/SkyBox.cpp
+++ Modules/Graphics/Extensions/Sources/Methane/Graphics/SkyBox.cpp
@@ -43,6 +43,36 @@ SkyBox::SkyBox(Context& context, ImageLoader& image_loader, const Settings& sett
     , m_mesh_buffers(context, BoxMesh<SkyBoxVertex>(Mesh::VertexLayoutFromArray(SkyBoxVertex::layout)), "Sky-Box")
 {
     m_mesh_buffers.SetTexture(image_loader.LoadImagesToTextureCube(m_context, m_settings.face_resources));
+
+    const Context::Settings& context_settings = context.GetSettings();
+
+    RenderState::Settings state_settings;
+    state_settings.sp_program = Program::Create(context, {
+        {
+            Shader::CreateVertex(context, { { "SkyBox", "SkyboxVS", "vs_5_1" }, { } }),
+            Shader::CreatePixel( context, { { "SkyBox", "SkyboxPS", "ps_5_1" }, { } }),
+        },
+        { { {
+            { "in_position", "POSITION" },
+        } } },
+        { "g_skybox_texture", "g_texture_sampler" },
+        { context_settings.color_format },
+        context_settings.depth_stencil_format
+    });
+    state_settings.sp_program->SetName("Sky-box shading");
+    state_settings.viewports     = { GetFrameViewport(context_settings.frame_size) };
+    state_settings.scissor_rects = { GetFrameScissorRect(context_settings.frame_size) };
+    state_settings.depth.enabled = false;
+    state_settings.rasterizer.is_front_counter_clockwise = true;
+
+    m_sp_state = RenderState::Create(context, state_settings);
+    m_sp_state->SetName("Sky-box render state");
+}
+
+void SkyBox::Draw(RenderCommandList& cmd_list, Program::ResourceBindings& resource_bindings)
+{
+    cmd_list.Reset(*m_sp_state, "Sky-box rendering");
+    m_mesh_buffers.Draw(cmd_list, resource_bindings, 1);
 }
 
 } // namespace Methane::Graphics
