fdab9999b2be6d03328593b162cf636d71065186
Evgeny Gorodetskiy <evsgor@gmail.com>
2019-12-22 13:34:39 +0000

    GitPod cloud-IDE configuration added.

--- Modules/Data/RangeSet/Test/Main.cpp
+++ Modules/Data/RangeSet/Test/Main.cpp
@@ -20,5 +20,6 @@ FILE: Test/Main.cpp
 
 ******************************************************************************/
 
+#define CATCH_CONFIG_CONSOLE_WIDTH 300
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
