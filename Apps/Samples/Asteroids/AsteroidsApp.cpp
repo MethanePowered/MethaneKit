3007c906b35121e34f2e0231d8eb8ce2939116c4
Evgeny Gorodetsky <evsgor@gmail.com>
2020-01-12 19:55:18 +0300

    Updated asteroids default camera orientation and added sample screenshot.

--- Apps/Samples/Asteroids/AsteroidsApp.cpp
+++ Apps/Samples/Asteroids/AsteroidsApp.cpp
@@ -147,7 +147,7 @@ AsteroidsApp::AsteroidsApp()
     // NOTE: Near and Far values are swapped in camera parameters (1st value is near = max depth, 2nd value is far = min depth)
     // for Reversed-Z buffer values range [ near: 1, far 0], instead of [ near 0, far 1]
     // which is used for "from near to far" drawing order for reducing pixels overdraw
-    m_view_camera.SetOrientation({ { 0.f, 60.f, 250.f }, { 0.f, 0.f, 0.f }, { 0.f, 1.f, 0.f } });
+    m_view_camera.SetOrientation({ { -110.f, 75.f, 210.f }, { 0.f, -60.f, 25.f }, { 0.f, 1.f, 0.f } });
     m_view_camera.SetParamters({ 600.f /* near = max depth */, 0.01f /*far = min depth*/, 90.f /* FOV */ });
     m_view_camera.SetZoomDistanceRange({ 60.f , 400.f });
 
