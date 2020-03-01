652bd439488b0a718912e288e63155401a472199
Evgeny Gorodetsky <evsgor@gmail.com>
2020-03-01 14:33:04 +0300

    Added copyright to shaders

--- Apps/Tutorials/03-ShadowCube/Shaders/ShadowCube.hlsl
+++ Apps/Tutorials/03-ShadowCube/Shaders/ShadowCube.hlsl
@@ -1,6 +1,29 @@
-// Optional macro definitions:
-// #define ENABLE_SHADOWS
-// #define ENABLE_TEXTURING
+/******************************************************************************
+
+Copyright 2019-2020 Evgeny Gorodetskiy
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*******************************************************************************
+
+FILE: MethaneKit/Apps/Tutorials/03-ShadowCube/Shaders/ShadowCube.hlsl
+Shaders for shadow and final pass rendering of textured cube
+with Phong lighting model and simple shadows
+
+Optional macro definition: ENABLE_SHADOWS, ENABLE_TEXTURING
+
+******************************************************************************/
+
 
 struct VSInput
 {
