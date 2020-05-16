4896b8514db20003e5a40963d66cf811fde69c59
Evgeny Gorodetsky <evsgor@gmail.com>
2020-05-16 23:34:58 +0300

    Improve Metal app view implementation on MacOS

--- Modules/Platform/AppView/Sources/Methane/Platform/MacOS/AppViewMT.mm
+++ Modules/Platform/AppView/Sources/Methane/Platform/MacOS/AppViewMT.mm
@@ -61,21 +61,23 @@ public:
     }
 
 private:
-    id<CAMetalDrawable> __strong m_drawable;
-    TRACY_GPU_SCOPE_TYPE         m_present_scope;
+    __strong id<CAMetalDrawable>  m_drawable;
+    TRACY_GPU_SCOPE_TYPE m_present_scope;
 };
 
 #endif
 
 @interface AppViewMT ()
 {
-    NSTrackingArea* m_tracking_area;
-    id<CAMetalDrawable> __strong m_current_drawable;
-    std::unique_ptr<Methane::Tracy::GpuContext> m_sp_gpu_context;
+    NSTrackingArea*              m_tracking_area;
+    __strong id<CAMetalDrawable> m_current_drawable;
+    CVDisplayLinkRef             m_display_link;
+    dispatch_source_t            m_display_source;
 
 #ifdef METHANE_GPU_INSTRUMENTATION_ENABLED
-    DrawablePresentScopeSet m_present_scopes;
-    std::mutex              m_present_scopes_mutex;
+    std::unique_ptr<Methane::Tracy::GpuContext> m_sp_gpu_context;
+    DrawablePresentScopeSet                     m_present_scopes;
+    std::mutex                                  m_present_scopes_mutex;
 #endif
 }
 
@@ -95,28 +97,6 @@ private:
 @synthesize unsyncRefreshInterval = m_unsync_refresh_interval;
 @synthesize redrawing = m_redrawing;
 
-CVDisplayLinkRef g_display_link;
-
-static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
-                                   const CVTimeStamp* /*now*/,
-                                   const CVTimeStamp* /*output_time*/,
-                                   CVOptionFlags /*flags_in*/,
-                                   CVOptionFlags* /*flags_out*/,
-                                   void* p_display_link_context)
-{
-    META_FUNCTION_TASK();
-
-    AppViewMT* app_view = (__bridge AppViewMT*) p_display_link_context;
-    
-    if (app_view.redrawing)
-    {
-        // Rendering in Main thread
-        [app_view performSelectorOnMainThread:@selector(redraw) withObject:nil waitUntilDone:YES];
-    }
-
-    return kCVReturnSuccess;
-}
-
 - (instancetype) initWithCoder:(NSCoder *)aDecoder
 {
     META_FUNCTION_TASK();
@@ -130,11 +110,6 @@ static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
         m_vsync_enabled = YES;
         m_unsync_refresh_interval = 1.0 / 120;
         m_current_drawable = nil;
-        m_sp_gpu_context = std::make_unique<Methane::Tracy::GpuContext>(
-            Methane::Tracy::GpuContext::Settings(
-                Methane::Data::ConvertTimeSecondsToNanoseconds(CACurrentMediaTime())
-            )
-        );
         [self commonInit];
     }
     
@@ -162,11 +137,6 @@ static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
         m_vsync_enabled = vsync_enabled;
         m_unsync_refresh_interval = refresh_interval_sec;
         m_current_drawable = nil;
-        m_sp_gpu_context = std::make_unique<Methane::Tracy::GpuContext>(
-            Methane::Tracy::GpuContext::Settings(
-                Methane::Data::ConvertTimeSecondsToNanoseconds(CACurrentMediaTime())
-            )
-        );
         [self updateTrackingAreas];
         [self commonInit];
     }
@@ -186,15 +156,91 @@ static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
 {
     META_FUNCTION_TASK();
 
+#ifdef METHANE_GPU_INSTRUMENTATION_ENABLED
+    m_sp_gpu_context = std::make_unique<Methane::Tracy::GpuContext>(
+        Methane::Tracy::GpuContext::Settings(
+            Methane::Data::ConvertTimeSecondsToNanoseconds(CACurrentMediaTime())
+        )
+    );
+#endif
+
     self.metalLayer.pixelFormat = self.pixelFormat;
     self.wantsLayer = YES;
     self.layerContentsRedrawPolicy = NSViewLayerContentsRedrawOnSetNeedsDisplay;
-    
+
+    // Register to be notified when the window closes so that you can stop the display link
     NSNotificationCenter* ns_notification_center = [NSNotificationCenter defaultCenter];
     [ns_notification_center addObserver:self
-                           selector:@selector(windowWillClose:)
-                               name:NSWindowWillCloseNotification
-                             object:self.window];
+                               selector:@selector(windowWillClose:)
+                                   name:NSWindowWillCloseNotification
+                                 object:self.window];
+}
+
+static CVReturn DispatchRenderLoop(CVDisplayLinkRef /*display_link*/,
+                                   const CVTimeStamp* /*now*/,
+                                   const CVTimeStamp* /*output_time*/,
+                                   CVOptionFlags /*flags_in*/,
+                                   CVOptionFlags* /*flags_out*/,
+                                   void* p_display_link_context)
+{
+    META_FUNCTION_TASK();
+    // 'DispatchRenderLoop' is always called on a secondary thread.
+    // Merge the dispatch source setup for the main queue so that rendering occurs on the main thread.
+    __weak dispatch_source_t source = (__bridge dispatch_source_t)p_display_link_context;
+    dispatch_source_merge_data(source, 1);
+    return kCVReturnSuccess;
+}
+
+- (BOOL) setupDisplayLinkForScreen:(NSScreen*) screen
+{
+    META_FUNCTION_TASK();
+
+    // The CVDisplayLink callback, DispatchRenderLoop, never executes
+    // on the main thread. To execute rendering on the main thread, create
+    // a dispatch source using the main queue (the main thread).
+    // DispatchRenderLoop merges this dispatch source in each call
+    // to execute rendering on the main thread.
+    m_display_source = dispatch_source_create(DISPATCH_SOURCE_TYPE_DATA_ADD, 0, 0, dispatch_get_main_queue());
+    __weak AppViewMT* p_weak_view = self;
+    dispatch_source_set_event_handler(m_display_source, ^(){
+        if (!p_weak_view.redrawing)
+            return;
+
+        @autoreleasepool
+        {
+            [p_weak_view redraw];
+        }
+    });
+    dispatch_resume(m_display_source);
+
+    // Create a display link capable of being used with all active displays
+    if (CVDisplayLinkCreateWithActiveCGDisplays(&m_display_link) != kCVReturnSuccess)
+        return NO;
+
+    // Set DispatchRenderLoop as the callback function and supply m_display_source as the argument to the callback.
+    if (CVDisplayLinkSetOutputCallback(m_display_link, &DispatchRenderLoop, (__bridge void*)m_display_source) != kCVReturnSuccess)
+        return NO;
+
+    // Associate the display link with the display on which the view resides
+    CGDirectDisplayID view_display_id = (CGDirectDisplayID) [self.window.screen.deviceDescription[@"NSScreenNumber"] unsignedIntegerValue];;
+    if(CVDisplayLinkSetCurrentCGDisplay(m_display_link, view_display_id) != kCVReturnSuccess)
+        return NO;
+
+    CVDisplayLinkStart(m_display_link);
+    return YES;
+}
+
+- (void) stopDisplayLink
+{
+    META_FUNCTION_TASK();
+    if (!m_display_link)
+        return;
+
+    [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+    CVDisplayLinkStop(m_display_link);
+    CVDisplayLinkRelease(m_display_link);
+    dispatch_source_cancel(m_display_source);
+    m_display_link = nil;
 }
 
 - (NSScreen*) currentScreen
@@ -228,27 +274,6 @@ static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
     return layer;
 }
 
-- (void) layout
-{
-    META_FUNCTION_TASK();
-
-    [super layout];
-    CGFloat scale = self.window ? self.window.screen.backingScaleFactor
-                                : [NSScreen mainScreen].backingScaleFactor;
-
-    CGSize drawable_size = self.bounds.size;
-    drawable_size.width *= scale;
-    drawable_size.height *= scale;
-
-    self.metalLayer.drawableSize = drawable_size;
-    m_current_drawable = nil;
-    
-    if ([self.delegate respondsToSelector:@selector(appView:drawableSizeWillChange:)])
-    {
-        [self.delegate appView: self drawableSizeWillChange: drawable_size];
-    }
-}
-
 - (id<CAMetalDrawable>) currentDrawable
 {
     META_FUNCTION_TASK();
@@ -269,6 +294,50 @@ static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
     return m_current_drawable;
 }
 
+- (void) resizeDrawable
+{
+    META_FUNCTION_TASK();
+    CGFloat scale = self.window ? self.window.screen.backingScaleFactor
+                                : [NSScreen mainScreen].backingScaleFactor;
+
+    CGSize drawable_size = self.bounds.size;
+    drawable_size.width  *= scale;
+    drawable_size.height *= scale;
+
+    self.metalLayer.drawableSize = drawable_size;
+    m_current_drawable = nil;
+
+    if ([self.delegate respondsToSelector:@selector(appView:drawableSizeWillChange:)])
+    {
+        [self.delegate appView: self drawableSizeWillChange: drawable_size];
+    }
+}
+
+- (void) viewDidMoveToWindow
+{
+    META_FUNCTION_TASK();
+    [super viewDidMoveToWindow];
+    if (self.vsyncEnabled && m_display_link)
+    {
+        [self setupDisplayLinkForScreen:self.currentScreen];
+    }
+    [self resizeDrawable];
+}
+
+- (void) layout
+{
+    META_FUNCTION_TASK();
+    [super layout];
+    [self resizeDrawable];
+}
+
+- (void)viewDidChangeBackingProperties
+{
+    META_FUNCTION_TASK();
+    [super viewDidChangeBackingProperties];
+    [self resizeDrawable];
+}
+
 - (void) setUnsyncRefreshInterval: (NSTimeInterval) unsync_refresh_interval
 {
     META_FUNCTION_TASK();
@@ -280,7 +349,11 @@ static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
     if (self.redrawing && !self.vsyncEnabled)
     {
         [self.unsyncTimer invalidate];
-        self.unsyncTimer = [NSTimer scheduledTimerWithTimeInterval:m_unsync_refresh_interval target:self selector:@selector(redraw) userInfo:nil repeats:YES];
+        self.unsyncTimer = [NSTimer scheduledTimerWithTimeInterval:m_unsync_refresh_interval
+                                                            target:self
+                                                          selector:@selector(redraw)
+                                                          userInfo:nil
+                                                           repeats:YES];
     }
 }
 
@@ -333,29 +406,21 @@ static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
     {
         if (m_vsync_enabled)
         {
-            CVReturn cv_return = CVDisplayLinkCreateWithActiveCGDisplays(&g_display_link);
-            assert(cv_return == kCVReturnSuccess);
-
-            cv_return = CVDisplayLinkSetOutputCallback(g_display_link, &OnDisplayLinkFrame, (__bridge void *)self);
-            assert(cv_return == kCVReturnSuccess);
-
-            cv_return = CVDisplayLinkSetCurrentCGDisplay(g_display_link, CGMainDisplayID());
-            assert(cv_return == kCVReturnSuccess);
-            
-            CVDisplayLinkStart(g_display_link);
+            [self setupDisplayLinkForScreen:self.currentScreen];
         }
         else
         {
             // Create non-sync refresh timer
-            self.unsyncTimer = [NSTimer scheduledTimerWithTimeInterval:m_unsync_refresh_interval target:self selector:@selector(redraw) userInfo:nil repeats:YES];
+            self.unsyncTimer = [NSTimer scheduledTimerWithTimeInterval:m_unsync_refresh_interval
+                                                                target:self
+                                                              selector:@selector(redraw)
+                                                              userInfo:nil
+                                                               repeats:YES];
         }
     }
-    else if (g_display_link)
+    else
     {
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-        CVDisplayLinkStop(g_display_link);
-        CVDisplayLinkRelease(g_display_link);
-        g_display_link = nil;
+        [self stopDisplayLink];
     }
 }
 
@@ -381,7 +446,7 @@ static CVReturn OnDisplayLinkFrame(CVDisplayLinkRef /*display_link*/,
 
     // Stop the display link when the window is closing because we will
     // not be able to get a drawable, but the display link may continue to fire
-    if (notification.object == self.window && g_display_link)
+    if (notification.object == self.window && m_display_link)
     {
         self.redrawing = NO;
     }
