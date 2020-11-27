5dc230202a2602b4aa91237775a9e8902c96006b
Evgeny Gorodetsky <evsgor@gmail.com>
2020-11-27 12:57:52 +0300

    Disable sonar check for constructor with variadic universal references to get rid of false positives about slicing

--- Modules/UserInterface/Types/Include/Methane/UserInterface/Types.hpp
+++ Modules/UserInterface/Types/Include/Methane/UserInterface/Types.hpp
@@ -67,8 +67,9 @@ struct UnitType : BaseType
     template<typename V>
     explicit UnitType(const UnitType<V>& other) noexcept : BaseType(static_cast<const V&>(other)), units(other.units) { }
 
+    // Disable Sonar Check for variadic arguments constructor, since it reports false positive about slicing for universal references
     template<typename... BaseArgs>
-    UnitType(Units units, BaseArgs&&... base_args) noexcept : BaseType(std::forward<BaseArgs>(base_args)...), units(units) { }
+    UnitType(Units units, BaseArgs&&... base_args) noexcept : BaseType(std::forward<BaseArgs>(base_args)...), units(units) { } //NOSONAR
 
     template<typename T = BaseType, typename = std::enable_if_t<std::is_same_v<FramePoint, T>>>
     explicit UnitType(const UnitType<FrameSize>& size) noexcept : UnitType<FramePoint>(size.units, size.width, size.height) { }
