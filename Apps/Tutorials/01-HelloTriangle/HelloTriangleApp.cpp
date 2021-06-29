7872134bdf3ee8b3b8f0bf42c610768e9839e3b3
Evgeny Gorodetsky <evsgor@gmail.com>
2021-06-29 09:02:50 +0300

    Fix syntax error in Hello Triangle tutorial

--- Apps/Tutorials/01-HelloTriangle/HelloTriangleApp.cpp
+++ Apps/Tutorials/01-HelloTriangle/HelloTriangleApp.cpp
@@ -85,7 +85,7 @@ public:
                         GetScreenPassPattern().GetAttachmentFormats()
                     }
                 ),
-                c
+                GetScreenPassPatternPtr()
             }
         );
 
