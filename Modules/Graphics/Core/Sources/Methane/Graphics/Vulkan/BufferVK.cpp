3ee566a66b4418c1e954037ad3a672d92818ac74
Evgeny Gorodetsky <evsgor@gmail.com>
2021-08-24 23:55:01 +0300

    Add Vulkan buffer creation

--- Modules/Graphics/Core/Sources/Methane/Graphics/Vulkan/BufferVK.cpp
+++ Modules/Graphics/Core/Sources/Methane/Graphics/Vulkan/BufferVK.cpp
@@ -22,6 +22,7 @@ Vulkan implementation of the buffer interface.
 ******************************************************************************/
 
 #include "BufferVK.h"
+#include "DeviceVK.h"
 
 #include <Methane/Graphics/Types.h>
 #include <Methane/Graphics/ContextBase.h>
@@ -71,11 +72,40 @@ Data::Size Buffer::GetAlignedBufferSize(Data::Size size) noexcept
     return size;
 }
 
+static vk::BufferUsageFlags GetVulkanBufferUsageFlags(Buffer::Type buffer_type)
+{
+    META_FUNCTION_TASK();
+    vk::BufferUsageFlags vk_usage_flags;
+    switch(buffer_type)
+    {
+    case Buffer::Type::Data:     vk_usage_flags |= vk::BufferUsageFlagBits::eStorageBuffer; break;
+    case Buffer::Type::Constant: vk_usage_flags |= vk::BufferUsageFlagBits::eUniformBuffer; break;
+    case Buffer::Type::Index:    vk_usage_flags |= vk::BufferUsageFlagBits::eIndexBuffer; break;
+    case Buffer::Type::Vertex:   vk_usage_flags |= vk::BufferUsageFlagBits::eVertexBuffer; break;
+    // Buffer::Type::ReadBack - unsupported
+    default: META_UNEXPECTED_ARG_DESCR(buffer_type, "Unsupported buffer type");
+    }
+    return vk_usage_flags;
+}
+
 BufferVK::BufferVK(const ContextBase& context, const Settings& settings, const DescriptorByUsage& descriptor_by_usage)
     : ResourceVK(context, settings, descriptor_by_usage)
+    , m_vk_device(dynamic_cast<const IContextVK&>(context).GetDeviceVK().GetNativeDevice())
+    , m_vk_buffer(m_vk_device.createBuffer(
+        vk::BufferCreateInfo(
+            vk::BufferCreateFlags{},
+            settings.size,
+            GetVulkanBufferUsageFlags(settings.type),
+            vk::SharingMode::eExclusive)))
 {
     META_FUNCTION_TASK();
     InitializeDefaultDescriptors();
+}
+
+BufferVK::~BufferVK()
+{
+    META_FUNCTION_TASK();
+    m_vk_device.destroy(m_vk_buffer);
 }
 
 void BufferVK::SetName(const std::string& name)
--- Modules/Graphics/Core/Sources/Methane/Graphics/Vulkan/BufferVK.h
+++ Modules/Graphics/Core/Sources/Methane/Graphics/Vulkan/BufferVK.h
@@ -28,6 +28,8 @@ Vulkan implementation of the buffer interface.
 #include <Methane/Graphics/BufferBase.h>
 #include <Methane/Graphics/Types.h>
 
+#include <vulkan/vulkan.hpp>
+
 namespace Methane::Graphics
 {
 
@@ -35,12 +37,17 @@ class BufferVK final : public ResourceVK<BufferBase>
 {
 public:
     BufferVK(const ContextBase& context, const Settings& settings, const DescriptorByUsage& descriptor_by_usage = DescriptorByUsage());
+    ~BufferVK() override;
 
     // Resource interface
     void SetData(const SubResources& sub_resources, CommandQueue* sync_cmd_queue) override;
 
     // Object interface
     void SetName(const std::string& name) override;
+
+private:
+    vk::Device m_vk_device;
+    vk::Buffer m_vk_buffer;
 };
 
 class BufferSetVK final : public BufferSetBase
