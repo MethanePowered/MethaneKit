9fc9ff77bf0bbcf8302ad979e9d06f7098ed6e0f
Evgeny Gorodetskiy <evsgor@gmail.com>
2022-07-11 00:11:23 +0300

    Fix Catch2 build errors on Linux with GCC <= 9

--- Externals/Catch2.cmake
+++ Externals/Catch2.cmake
@@ -5,3 +5,10 @@ CPMAddPackage(
 )
 
 list(APPEND CMAKE_MODULE_PATH "${Catch2_SOURCE_DIR}/extras")
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU") # GCC
+    target_compile_options(Catch2
+        PUBLIC
+            -Wno-error=parentheses
+    )
+endif()
