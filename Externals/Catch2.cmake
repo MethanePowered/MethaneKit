f9abf8cc91eb94fd79868aa0d0b10f4882a18013
Evgeny Gorodetskiy <evsgor@gmail.com>
2022-07-11 12:05:14 +0300

    Fix Linux scan build, add logging to Windows tests executor in Azure Pipelines

--- Externals/Catch2.cmake
+++ Externals/Catch2.cmake
@@ -6,9 +6,10 @@ CPMAddPackage(
 
 list(APPEND CMAKE_MODULE_PATH "${Catch2_SOURCE_DIR}/extras")
 
+# Catch2 headers produce build errors with GCC <= 9, which are muted here
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU") # GCC
     target_compile_options(Catch2
         PUBLIC
-            -Wno-error=parentheses
+            -Wno-parentheses
     )
 endif()
