e49935419c85af3549294b81112c0f053957ba62
Evgeny Gorodetskiy <evsgor@gmail.com>
2023-04-05 23:25:22 +0300

    Add Graphics RHI unit-tests

---
+++ Tests/Graphics/RHI/ComputeContextTest.cpp
@@ -0,0 +1,64 @@
+/******************************************************************************
+
+Copyright 2023 Evgeny Gorodetskiy
+
+Licensed under the Apache License, Version 2.0 (the "License"),
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*******************************************************************************
+
+FILE: Tests/Graphics/RHI/ColorTest.cpp
+Unit-tests of the RHI ComputeContext
+
+******************************************************************************/
+
+#include "Methane/Graphics/RHI/IComputeContext.h"
+#include <Methane/Graphics/RHI/ComputeContext.h>
+#include <Methane/Graphics/RHI/System.h>
+#include <Methane/Graphics/RHI/Device.h>
+
+#include <stdexcept>
+#include <taskflow/taskflow.hpp>
+
+using namespace Methane::Graphics;
+
+static tf::Executor g_parallel_executor;
+
+static Rhi::Device GetTestDevice()
+{
+    const Rhi::Devices& devices = Rhi::System::Get().UpdateGpuDevices();
+    if (devices.empty())
+        throw std::logic_error("No RHI devices available");
+    return devices[0];
+}
+
+bool operator==(const Rhi::ComputeContextSettings& left, const Rhi::ComputeContextSettings& right)
+{
+    return left.options == right.options;
+}
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("RHI Compute Context", "[rhi][compute][context]")
+{
+    const Rhi::ComputeContextSettings compute_context_settings{
+        Rhi::ContextOptionMask{ Rhi::ContextOption::TransferWithD3D12DirectQueue }
+    };
+    const Rhi::ComputeContext compute_context(GetTestDevice(), g_parallel_executor, compute_context_settings);
+
+    SECTION("Check initialization")
+    {
+        CHECK(compute_context.IsInitialized());
+        CHECK(compute_context.GetInterfacePtr());
+        CHECK(compute_context.GetInterface().GetSettings() == compute_context_settings);
+    }
+}
