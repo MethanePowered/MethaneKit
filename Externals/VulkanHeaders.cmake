20778767ee01b3a88ae0ddc6cd72ffb3fc7bd334
Evgeny Gorodetskiy <evsgor@gmail.com>
2023-07-26 00:45:14 +0300

    Fix Vulkan::Headers CMake target duplication when VULKAN_SDK path is defined

--- Externals/VulkanHeaders.cmake
+++ Externals/VulkanHeaders.cmake
@@ -1,3 +1,8 @@
+if(TARGET Vulkan::Headers)
+    # Vulkan::Headers target can be added by find_package(Vulkan) when VULKAN_SDK environment variable defines valid path
+    return()
+endif()
+
 CPMAddPackage(
     NAME VulkanHeaders
     GITHUB_REPOSITORY MethanePowered/VulkanHeaders
