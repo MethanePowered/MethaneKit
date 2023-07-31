0d3553e057b627d71187e9aeee0e3cd628d79203
Evgeny Gorodetskiy <evsgor@gmail.com>
2023-07-31 23:25:37 +0300

    Fix clang warning

--- Apps/07-ParallelRendering/ParallelRenderingApp.cpp
+++ Apps/07-ParallelRendering/ParallelRenderingApp.cpp
@@ -205,7 +205,7 @@ void ParallelRenderingApp::Init()
         frame.cubes_array.program_bindings_per_instance[0].SetName(fmt::format("Cube 0 Bindings {}", frame.index));
 
         program_bindings_task_flow.for_each_index(1U, cubes_count, 1U,
-            [this, &frame, uniform_data_size](const uint32_t cube_index)
+            [this, &frame](const uint32_t cube_index)
             {
                 rhi::ProgramBindings& cube_program_bindings = frame.cubes_array.program_bindings_per_instance[cube_index];
                 cube_program_bindings = rhi::ProgramBindings(frame.cubes_array.program_bindings_per_instance[0], {
