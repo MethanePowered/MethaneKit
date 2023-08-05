38a656cc5254d90c35410b5c411ce13681b5918f
Evgeny Gorodetsky <evsgor@gmail.com>
2023-08-05 19:21:21 +0300

    Fix build errors

--- Externals/STB.cmake
+++ Externals/STB.cmake
@@ -6,3 +6,9 @@ CPMAddPackage(
 
 add_library(STB INTERFACE)
 target_include_directories(STB INTERFACE "${STB_SOURCE_DIR}")
+
+if(MSVC)
+    target_compile_options(STB INTERFACE
+        /wd4244 # conversion from 'int' to 'short', possible loss of data (stb_image.h:2224)
+    )
+endif() # MSVC
--- Externals/TaskFlow.cmake
+++ Externals/TaskFlow.cmake
@@ -5,6 +5,7 @@ CPMAddPackage(
     OPTIONS
         "TF_BUILD_BENCHMARKS OFF"
         "TF_BUILD_CUDA OFF"
+        "TF_BUILD_SYCL OFF"
         "TF_BUILD_TESTS OFF"
         "TF_BUILD_EXAMPLES OFF"
 )
@@ -14,6 +15,8 @@ add_library(TaskFlow ALIAS Taskflow)
 if(MSVC)
     target_compile_options(Taskflow INTERFACE
         /wd4456 # declaration of 'lock' hides previous local declaration (taskflow/core/executor.hpp:1842)
+        /wd4267 # conversion from 'size_t' to 'unsigned char', possible loss of data (taskflow/algorithm/sort.hpp:229)
+        /wd4146 # unary minus operator applied to unsigned type, result still unsigned (taskflow/algorithm/sort.hpp:42)
     )
 else() # Clang or GCC
     target_compile_options(Taskflow INTERFACE
