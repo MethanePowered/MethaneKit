a322bca680e060bc6285fb4bef2455fdd2a34eda
Evgeny Gorodetskiy <evsgor@gmail.com>
2023-09-21 15:27:20 +0300

    Extend RHI Fence test with value checks

--- Tests/Graphics/RHI/FenceTest.cpp
+++ Tests/Graphics/RHI/FenceTest.cpp
@@ -28,6 +28,7 @@ Unit-tests of the RHI Fence
 #include <Methane/Graphics/RHI/CommandQueue.h>
 #include <Methane/Graphics/RHI/CommandKit.h>
 #include <Methane/Graphics/RHI/Fence.h>
+#include <Methane/Graphics/Null/Fence.h>
 
 #include <memory>
 #include <taskflow/taskflow.hpp>
@@ -37,6 +38,11 @@ using namespace Methane;
 using namespace Methane::Graphics;
 
 static tf::Executor g_parallel_executor;
+
+static uint64_t GetFenceValue(const Rhi::Fence& fence)
+{
+    return dynamic_cast<Null::Fence&>(fence.GetInterface()).GetValue();
+}
 
 TEST_CASE("RHI Fence Functions", "[rhi][queue]")
 {
@@ -88,7 +94,9 @@ TEST_CASE("RHI Fence Functions", "[rhi][queue]")
 
     SECTION("Signal Fence")
     {
+        const uint64_t value = GetFenceValue(fence);
         CHECK_NOTHROW(fence.Signal());
+        CHECK(GetFenceValue(fence) == value + 1);
     }
 
     SECTION("Wait on CPU")
@@ -101,13 +109,27 @@ TEST_CASE("RHI Fence Functions", "[rhi][queue]")
         CHECK_NOTHROW(fence.WaitOnGpu(compute_context.GetUploadCommandKit().GetQueue()));
     }
 
+    SECTION("Can not wait on GPU with the same queue")
+    {
+        CHECK_THROWS(fence.WaitOnGpu(compute_cmd_queue));
+    }
+
     SECTION("Flush on CPU")
     {
+        const uint64_t value = GetFenceValue(fence);
         CHECK_NOTHROW(fence.FlushOnCpu());
+        CHECK(GetFenceValue(fence) == value + 1);
     }
 
     SECTION("Flush on GPU")
     {
+        const uint64_t value = GetFenceValue(fence);
         CHECK_NOTHROW(fence.FlushOnGpu(compute_context.GetUploadCommandKit().GetQueue()));
+        CHECK(GetFenceValue(fence) == value + 1);
+    }
+
+    SECTION("Can not flush on GPU with the same queue")
+    {
+        CHECK_THROWS(fence.FlushOnGpu(compute_cmd_queue));
     }
 }
