cc9d23d82a5b5623e0ba9928156a6b2bb682b3a2
Evgeny Gorodetskiy <evsgor@gmail.com>
2024-08-03 18:02:41 +0300

    Update CMakeModules with fix for ARM64 architecture detection from VS2022 and Ninja in FindWindowsSDK.cmake

--- Externals/CMakeModules.cmake
+++ Externals/CMakeModules.cmake
@@ -1,7 +1,7 @@
 CPMAddPackage(
     NAME CMakeModules
     GITHUB_REPOSITORY MethanePowered/CMakeModules
-    GIT_TAG 4ea2e62d063fe4a7e2e28c03d2772537c3a7dccc # last commit from 'methane' branch
+    GIT_TAG 09b04c9d113cd3a5ed49711999de68896e352c60 # last commit from 'methane' branch
     DOWNLOAD_ONLY YES
 )
 
