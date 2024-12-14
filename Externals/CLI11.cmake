11e2bfa92ad8bd3d323690e406e2a14ae8f4bf0d
Evgeny Gorodetskiy <evsgor@gmail.com>
2024-12-14 21:43:58 +0300

    Update CLI11 external repo to latest mainline

--- Externals/CLI11.cmake
+++ Externals/CLI11.cmake
@@ -1,7 +1,7 @@
 CPMAddPackage(
     NAME CLI11
     GITHUB_REPOSITORY MethanePowered/CLI11
-    VERSION 2.3.2
+    GIT_TAG a4e5560c5d05bef5dac0c948fb305195c3b8c254 # main branch commit from 10.12.2024
 )
 
 set_target_properties(CLI11
