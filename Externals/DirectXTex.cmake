f3316341a2e810bcdd13044658fe34a4517df1f4
Evgeny Gorodetsky <evsgor@gmail.com>
2024-12-15 22:07:54 +0300

    Update DirectXTex external repo to v2.0.6 (oct 2024)

--- Externals/DirectXTex.cmake
+++ Externals/DirectXTex.cmake
@@ -1,8 +1,8 @@
 CPMAddPackage(
     NAME DirectXTex
     GITHUB_REPOSITORY MethanePowered/DirectXTex
-    GIT_TAG may2022
-    VERSION 1.9.6
+    GIT_TAG oct2024
+    VERSION 2.0.6
     OPTIONS
         "BUILD_DX12 ON"
         "BUILD_DX11 OFF"
